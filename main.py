daf602299a1cc75ebb19a267ff089b94c16785d5
likov <likov.shura2003@mail.ru>
2023-11-07 22:19:31 +0700

    new november

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("hello")
-print(" ")
-print("good")
+from flask import Flask
+app = Flask(__name__)
+n1 = '\n'
+@app.route("/")
+def main_page():
+    return "Hello"
+
+
+@app.route("/profile/")
+def page_pers():
+    return f'Имя кандидата: Позиция кандидата: Навыки кандидата: '
+
+app.run()
